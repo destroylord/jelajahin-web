08154f5b0dc722f22b7347ff41056524a2ec1f71
savinasz <savinazahro23@gmail.com>
2021-12-02 22:39:12 +0700

    logout.php

---
+++ avatar.php
@@ -0,0 +1,17 @@
+<li class="nav-item dropdown">
+	<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
+		<i class="align-middle" data-feather="settings"></i>
+	</a>
+	<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
+		<img src="../img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="Savina Zahro" /> <span class="align-middle" style="color: black;">Savina Zahro</span>
+	</a>
+	<div class="dropdown-menu dropdown-menu-end">
+		<div class="dropdown-divider"></div>
+		<a href="../logout.php"class="dropdown-item" 
+		<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
+		<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
+		<div class="dropdown-divider"></div>
+		<a href="pages-sign-in.php"class="dropdown-item" 
+		>Log out</a>
+	</div>
+</li>
